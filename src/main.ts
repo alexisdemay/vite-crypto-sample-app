addea73b7c76f78a53a4067fff018d51fb1bf995
Alexis Demay <alexis.demay@cacd2.fr>
2021-06-28 00:26:30 +0200

    :wrench: Update analytics

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,36 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router, { RouteNames, routerPush } from './router';
 import store from './store';
+
+import './index.css';
+
 import Analytics from 'analytics';
 import googleAnalytics from '@analytics/google-analytics';
 import perfumePlugin from '@analytics/perfumejs';
 import Perfume from 'perfume.js';
 
-import './index.css';
+const analytics = Analytics({
+  app: 'vite-crypto-sample-app',
+  plugins: [
+    {
+      name: 'custom-plugin',
+      track: ({ payload }) => {
+        console.log('paypload:', payload);
+      },
+    },
+    googleAnalytics({
+      trackingId: 'G-MVY0HQDEML',
+      anonymizeIp: true
+    }),
+    perfumePlugin({
+      category: 'performance',
+      perfume: Perfume,
+      destinations: {
+        'google-analytics': true,
+      },
+    }),
+  ],
+});
 
 const app = createApp(App);
 
@@ -15,19 +39,6 @@ app.use(router);
 app.use(store);
 
 routerPush(RouteNames.DASHBOARD);
-
-const analytics = Analytics({
-  app: 'vite-crypto-sample-app',
-  plugins: [
-    googleAnalytics({
-      trackingId: 'G-MVY0HQDEML',
-    }),
-    perfumePlugin({
-      category: 'performance',
-      perfume: Perfume,
-    }),
-  ],
-});
 
 function renderApp() {
   app.mount('#app');
